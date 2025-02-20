69fdad6ce920ffdf583c9a97156ebd7d2b01a9b3
Juan Pedro Martin <juamp_m@hotmail.com>
2025-02-20 21:58:59 +0100

    perf: prepare for first public release v1.0.0-beta

---
+++ examples/basic.ts
@@ -0,0 +1,10 @@
+import { Client } from "popads-sdk";
+
+const client = new Client(process.env.POPADS_API_KEY as string);
+
+async function main() {
+  const campaigns = await client.campaign.getCampaign(123);
+  console.log(campaigns);
+}
+
+main().catch(console.error);
