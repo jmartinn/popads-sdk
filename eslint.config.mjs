0355ee15d4cfaa499de84d5c302932877e7d052a
Juan Pedro Martin <juamp_m@hotmail.com>
2025-03-26 12:04:28 +0100

    chore: update eslint rules for better dx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import tseslint from "typescript-eslint";
-import importPlugin from "eslint-plugin-import";
-import prettierRecommended from "eslint-plugin-prettier/recommended";
-import globals from "globals";
-import js from "@eslint/js";
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default tseslint.config(
   js.configs.recommended,
   prettierRecommended,
   tseslint.configs.recommended,
   {
-    files: ["**/*.ts"],
-    ignores: ["**/dist/", "vitest.config.ts"],
+    files: ['**/*.ts'],
+    ignores: ['**/dist/', 'vitest.config.ts', '**/examples/*'],
     extends: [
       importPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.typescript,
@@ -18,25 +18,58 @@ export default tseslint.config(
     languageOptions: {
       globals: {
         ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ['./tsconfig.json'],
+        },
       },
     },
     rules: {
-      "import/order": [
-        "error",
+      'sort-imports': [
+        'error',
         {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["sibling", "parent"],
-            "index",
-            "object",
-          ],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true },
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true,
         },
       ],
-      "@typescript-eslint/no-unused-vars": "off",
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'builtin',
+            'external',
+            'internal',
+            ['sibling', 'parent'],
+            'index',
+            'object',
+          ],
+          'newlines-between': 'always',
+          'alphabetize': { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
